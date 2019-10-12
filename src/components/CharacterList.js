f2b2e934d85c15cbd1515c87164e610ac04e1541
Brian Hague <alphaseinor@gmail.com>
2019-10-11 22:33:55 -0500

    fixed bug where multiple searches would break the system

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -4,17 +4,19 @@ import axios from "axios";
 import {Row} from "reactstrap"
 import SearchForm from "./SearchForm";
 
+
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [characters, setCharacters] = useState([]);
-  const [query, setQuery] = useState(['https://rickandmortyapi.com/api/character'])
+  const defaultURL = 'https://rickandmortyapi.com/api/character'
+  const [query, setQuery] = useState(defaultURL)
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
 
     axios
-      .get(query)
+      .get(defaultURL)
       .then(response => {
         console.log(response.data.results)
         setCharacters(response.data.results)
@@ -28,11 +30,12 @@ export default function CharacterList() {
   //handler for query
 
   const modifyQuery = q => {
-    //console.log(`Query: ${q}`);
-    setQuery(`${query}/?name=${q}`)
-    //console.log(`New Query ${query}`)
+    console.log(`Query: ${q}`);
+    setQuery(`${defaultURL}/?name=${q}`)
+    console.log(`New Query ${query}`)
+
     axios
-      .get(`${query}/?name=${q}`)
+      .get(`${defaultURL}/?name=${q}`)
       .then(response => {
         console.log(response.data.results)
         setCharacters(response.data.results)
